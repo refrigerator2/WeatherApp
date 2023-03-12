e75c8c02f704cf9bf985a53cd78655b35c4d7dba
refridgerator2 <102374780+refrigerator2@users.noreply.github.com>
2023-03-12 10:16:06 +0300

    Update projectlogic.js

--- projectlogic.js
+++ projectlogic.js
@@ -1,12 +1,8 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '6fef55a042msh2bf54b76ca073d9p1b5f64jsn5c6b23b8f787',
-		'X-RapidAPI-Host': 'forecast9.p.rapidapi.com'
-	}
-};
 
-fetch('https://forecast9.p.rapidapi.com/', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+const input = document.getElementsByClassName("city");
+const city = "London"
+const Url = `http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}`
+async function request(myUrl){
+	await fetch(myUrl)
+} 
+	request(Url); 
