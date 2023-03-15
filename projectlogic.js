d6430aa1b5d7a45c2bce3613b4835f757aa3ec90
refridgerator2 <102374780+refrigerator2@users.noreply.github.com>
2023-03-15 22:59:36 +0300

    Update projectlogic.js

--- projectlogic.js
+++ projectlogic.js
@@ -1,8 +1,20 @@
+const akey = 'de7610c21cac8617562a8bd820aed8e6';
 
-const input = document.getElementsByClassName("city");
-const city = "London"
-const Url = `http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}`
-async function request(myUrl){
-	await fetch(myUrl)
-} 
-	request(Url); 
+	const lat = document.getElementsByClassName("lat").value;
+	const lon = document.getElementsByClassName("lon").value;
+	const btn = document.getElementsByClassName("btn")[0];
+	const time = "hourly";
+	const lang = "en"
+	function getLL(){
+		return lat, lon;
+	}
+	btn.addEventListener('click', getLL)
+
+	 const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${akey}`;
+	// const url = `https://api.openweathermap.org/data/3.0/onecall/timemachine?lat=39.099724&lon=-94.578331&dt=1643803200&appid=de7610c21cac8617562a8bd820aed8e6`;
+
+	function getData(myUrl){
+		return myUrl;
+	}
+	getData(url).then(response=>response.json).then(json=>console.log(json))
+		
