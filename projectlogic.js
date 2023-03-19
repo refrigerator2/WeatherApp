ea1ea807855cb1cb6149a525729d3067a28258b5
refridgerator2 <102374780+refrigerator2@users.noreply.github.com>
2023-03-19 20:53:14 +0300

    Update projectlogic.js

--- projectlogic.js
+++ projectlogic.js
@@ -1,20 +1,23 @@
-const akey = 'de7610c21cac8617562a8bd820aed8e6';
 
 	const lat = document.getElementsByClassName("lat").value;
 	const lon = document.getElementsByClassName("lon").value;
 	const btn = document.getElementsByClassName("btn")[0];
-	const time = "hourly";
-	const lang = "en"
+
+	const akey = 'a412a1788e338d0074a61750c01f41b8';
+	const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${akey}`;
+
 	function getLL(){
 		return lat, lon;
 	}
-	btn.addEventListener('click', getLL)
-
-	 const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${akey}`;
-	// const url = `https://api.openweathermap.org/data/3.0/onecall/timemachine?lat=39.099724&lon=-94.578331&dt=1643803200&appid=de7610c21cac8617562a8bd820aed8e6`;
-
-	function getData(myUrl){
-		return myUrl;
-	}
-	getData(url).then(response=>response.json).then(json=>console.log(json))
+	btn.addEventListener('click', function(){
+		fetch(url)
+	})
+	 
+	const languageSelector = document.getElementById('language');
+	languageSelector.addEventListener('change', function() {
+    const selectedLanguage = languageSelector.value;
+    const script = document.createElement('script');
+    script.src = `lang_${selectedLanguage}.js`;
+    document.head.appendChild(script);
+});
 		
