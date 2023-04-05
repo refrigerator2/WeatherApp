aa4a18239df1751ba86b62ae5c2f156d01cc9900
refridgerator2 <102374780+refrigerator2@users.noreply.github.com>
2023-04-05 19:52:39 +0300

    Update projectlogic.js

--- projectlogic.js
+++ projectlogic.js
@@ -1,54 +1,65 @@
 
-	const rep = document.getElementById('rep')
-	const city = document.getElementById('city');
-	const btn = document.getElementById("btn");
-	
-	function createContent(data){
-		// const div = document.createElement('div')
-		// console.log(data)
-		// const wt = data.list[0].main.temp
-		// div.innerText= wt;
-		// const p = document.createElement('p')
-		// p.setAttribute('id', "box")
-		// rep.append(p)
-		// p.append(div)
-		for(let i= 0;i<2;i++){
-			
-			// let wt
-			// switch(i){
-			// 	case i === 0:
-			// 		wt = temp
-			// 	case i === 1:
-			// 	    wt=feels_like
-			// 	case i === 2:
-			// 		wt=humidity
-			// }
-			const request = data.list[0].main.temp
-			const div = document.createElement('div')
-			div.innerText= request;
-			const p = document.createElement('p')
-			p.setAttribute('id', "box")
-			rep.append(p)
-			p.append(div)
+const rep = document.getElementById('rep')
+const city = document.getElementById('city');
+const btn = document.getElementById("btn");
+const tm = document.getElementById('tempcheck')
+
+
+function createContent(request){
+		const div = document.createElement('div')
+		div.innerText= request;
+		const p = document.createElement('p')
+		p.setAttribute('id', "box")
+		rep.append(p)
+		p.append(div)
+}
+function postContent(data){
+	const tempcheck = document.getElementById('tempcheck')
+	const humcheck = document.getElementById('humcheck')
+	const sunrise = document.getElementById('sunrise')
+	const sunset = document.getElementById('sunrise')
+	const press = document.getElementById('presscheck') 
+	const request = data.list[0].main
+	let value
+		if(tempcheck.checked){
+			value = request.temp
+			createContent(value)
 		}
-	}
+		if(humcheck.checked){
+			value = request.humidity
+			createContent(value)
+		}
+		 if(press.checked){
+			value = request.pressure
+		 	createContent(value)
+		 }
+		 if(sunrise.checked){
+			value = data.city.sunrise
+			createContent(value)
+		 }
+		 if(sunset.checked){
+			value = data.city.sunset
+			createContent(value)
+		 }
+		console.log(data)
+		}
 
-	async function getData(){
-	const akey = 'a412a1788e338d0074a61750c01f41b8';
-	const value = city.value
-	const url = `https://api.openweathermap.org/data/2.5/forecast?q=${value}&appid=${akey}`;
-	const dat = await fetch(url)
-		return dat;
-	}
+async function getData(){
+const akey = 'a412a1788e338d0074a61750c01f41b8';
+const value = city.value
+const url = `https://api.openweathermap.org/data/2.5/forecast?q=${value}&appid=${akey}`;
+const dat = await fetch(url)
+	return dat;
+}
 
-	function postData(){
-		getData()
-		.then(function(response){
-			response.json()
-			.then(function(data) {
-			createContent(data)
-		});
-	})
-	}
+function postData(){
+	getData()
+	.then(function(response){
+		response.json()
+		.then(function(data) {
+		postContent(data)
+	});
+})
+}
 
-	btn.addEventListener('click', postData)
+btn.addEventListener('click', postData)
