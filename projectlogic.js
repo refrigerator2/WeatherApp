4a1d4901fa2cad474ac733c83de90cb386f5df13
refridgerator2 <102374780+refrigerator2@users.noreply.github.com>
2023-04-10 22:51:15 +0300

    Update projectlogic.js

--- projectlogic.js
+++ projectlogic.js
@@ -3,16 +3,23 @@ const rep = document.getElementById('rep')
 const city = document.getElementById('city');
 const btn = document.getElementById("btn");
 const tm = document.getElementById('tempcheck')
+const unit = document.getElementById('units')
+
+let units;
+
+unit.addEventListener('change', function(event){
+	units = event.target.value
+	console.log(units)
+})
 
 
 function createContent(request){
 		const div = document.createElement('div')
 		div.innerText= request;
-		// const p = document.createElement('p')
 		div.setAttribute('id', "box")
-		// rep.append(p)
 		rep.append(div)
 }
+
 function postContent(data){
 	const tempcheck = document.getElementById('tempcheck')
 	const humcheck = document.getElementById('humcheck')
@@ -33,8 +40,12 @@ function postContent(data){
 			value = request.pressure
 		 	createContent(value)
 		 }
-		 if(sunrise.checked){
-			value = (data.city.sunrise / 3600)
+		 if(wind.checked){
+			value = data.list[0].wind.deg
+			createContent(value)
+			value = data.list[0].wind.speed 
+			createContent(value)
+			value = data.list[0].wind.gust 
 			createContent(value)
 		 }
 		value = data.list[0].weather.icon
@@ -44,7 +55,7 @@ function postContent(data){
 async function getData(){
 const akey = 'a412a1788e338d0074a61750c01f41b8';
 const value = city.value
-const url = `https://api.openweathermap.org/data/2.5/forecast?q=${value}&units=metric&appid=${akey}`;
+const url = `https://api.openweathermap.org/data/2.5/forecast?q=${value}&units=${units}&appid=${akey}`;
 const dat = await fetch(url)
 	return dat;
 }
