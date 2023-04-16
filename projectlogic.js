bd48f1fea5d67e4fb653a3d49e0266bd28381239
refridgerator2 <102374780+refrigerator2@users.noreply.github.com>
2023-04-16 14:12:18 +0300

    Update projectlogic.js

--- projectlogic.js
+++ projectlogic.js
@@ -1,3 +1,4 @@
+
 const tempcheck = document.getElementById('tempcheck')
 const humcheck = document.getElementById('humcheck')
 const press = document.getElementById('presscheck') 
@@ -7,7 +8,7 @@ const btn = document.getElementById("btn");
 const tm = document.getElementById('tempcheck')
 const unit = document.getElementById('units')
 
-let units;
+let units = "metric";
 
 unit.addEventListener('change', function(event){
 	units = event.target.value
@@ -23,101 +24,103 @@ function createContent(request){
 
 function measurementSystem(data, value){
 	const request = data.list[0].main
-switch (units){
-case "metric":
-	switch (value){
-		case request.temp:
-			value += "&#176"+"C"
-		break;
-		case request.humidity:
-			value+="%"
-		break;
-		case request.pressure:
-			value+="hPa"
+	switch (units){
+		case "metric":
+			switch (value){
+				case request.temp:
+					value += `°`+"C"
+					break;
+				case request.humidity:
+					value+="%"
+					break;
+				case request.pressure:
+					value+="hPa"
+					break;
+				case data.list[0].wind.deg:
+					value+="°"
+					break;
+				case data.list[0].wind.speed:
+					value+="meter/second"
+					break;
+				case data.list[0].wind.gust:
+					value+="meter/second"
+					break;
+			}
 			break;
-		case data.list[0].wind.deg:
-			value+="&#176"
-		break;
-		case data.list[0].wind.speed:
-			value+="meter/second"
-		break;
-		case data.list[0].wind.gust:
-			value+="meter/second"
-		break;
-	}
-	break;
-case "imperial":
-	switch (value){
-		case request.temp:
-			value += "&#176"+"F"
-		break;
-		case request.humidity:
-			value+="%"
-		break;
-		case request.pressure:
-			value+="hPa"
+		case "imperial":
+			switch (value){
+				case request.temp:
+					value+="°F"
+					break;
+				case request.humidity:
+					value+="%"
+					break;
+				case request.pressure:
+					value+="hPa"
+					break;
+				case data.list[0].wind.deg:
+					value+='°'
+					break;
+				case data.list[0].wind.speed:
+					value+="miles/hour"
+					break;
+				case data.list[0].wind.gust:
+					value+="miles/hour"
+					break;
+			}
 			break;
-		case data.list[0].wind.deg:
-			value+="&#176"
-		break;
-		case data.list[0].wind.speed:
-			value+="miles/hour"
-		break;
-		case data.list[0].wind.gust:
-			value+="miles/hour"
-		break;
-	}
-	break;
-	case "standart":
-	switch (value){
-		case request.temp:
-			value += "K"
-		break;
-		case request.humidity:
-			value+="%"
-		break;
-		case request.pressure:
-			value+="hPa"
+		case "standard":
+			switch (value){
+				case request.temp:
+					value+="K"
+					break;
+				case request.humidity:
+					value+="%"
+					break;
+				case request.pressure:
+					value+="hPa"
+					break;
+				case data.list[0].wind.deg:
+					value+="°"
+					break;
+				case data.list[0].wind.speed:
+					value+="meter/second"
+					break;
+				case data.list[0].wind.gust:
+					value+="meter/second"
+					break;
+			}
 			break;
-		case data.list[0].wind.deg:
-			value+="&#176"
-		break;
-		case data.list[0].wind.speed:
-			value+="meter/second"
-		break;
-		case data.list[0].wind.gust:
-			value+="meter/second"
-		break;
 	}
-	break;
+	return value;
 }
-}
+
 function postContent(data){
 	const request = data.list[0].main
 	let value
-		if(tempcheck.checked){
-			value = measurementSystem(data, request.temp)
-			createContent(value)
-		}
-		if(humcheck.checked){
-			value = request.humidity
-			createContent(value)
-		}
-		 if(press.checked){
-			value = request.pressure
-		 	createContent(value)
-		 }
-		 if(wind.checked){
-			value = data.list[0].wind.deg
-			createContent(value)
-			value = data.list[0].wind.speed 
-			createContent(value)
-			value = data.list[0].wind.gust 
-			createContent(value)
-		 }
-		value = data.list[0].weather.icon
+	if(tempcheck.checked){
+		value = measurementSystem(data, request.temp)	
 		createContent(value)
-		}
+	}
+	if(humcheck.checked){
+		value = measurementSystem(data, request.humidity)
+		createContent(value)
+	}
+	if(press.checked){
+		value = measurementSystem(data, request.pressure)
+		createContent(value)
+	}
+	if(wind.checked){
+		value = measurementSystem(data, data.list[0].wind.deg)
+		createContent(value)
+		value = measurementSystem(data, data.list[0].wind.speed) 
+		createContent(value)
+		value = measurementSystem(data, data.list[0].wind.gust)
+		createContent(value)
+	}
+	value = data.list[0].weather.icon
+	createContent(value)
+}
 
 async function getData(){
 const akey = 'a412a1788e338d0074a61750c01f41b8';
